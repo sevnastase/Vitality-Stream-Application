773bf2ff3bec03b300c40afb8ab27d866a3c9382
Ricky van Rijn <rickyvanrijn@gmail.com>
2021-01-13 17:02:06 +0100

    Added click event to profile, visualisation of selected profile.

--- app/src/main/java/com/videostreamtest/ui/phone/profiles/ProfileViewHolder.java
+++ app/src/main/java/com/videostreamtest/ui/phone/profiles/ProfileViewHolder.java
@@ -1,6 +1,10 @@
 package com.videostreamtest.ui.phone.profiles;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -12,6 +16,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.squareup.picasso.Picasso;
 import com.videostreamtest.R;
 import com.videostreamtest.data.model.Profile;
+import com.videostreamtest.ui.phone.catalog.CatalogActivity;
 
 public class ProfileViewHolder extends RecyclerView.ViewHolder {
     private ImageButton profileImageButton;
@@ -33,9 +38,30 @@ public class ProfileViewHolder extends RecyclerView.ViewHolder {
                 .placeholder(R.drawable.cast_album_art_placeholder)
                 .error(R.drawable.cast_ic_notification_disconnect)
                 .into(profileImageButton);
+
+        if (position == 0) {
+            final Drawable border = itemView.getContext().getDrawable(R.drawable.imagebutton_blue_border);
+            profileImageButton.setBackground(border);
+            profileImageButton.setAlpha(1.0f);
+        }
+        profileImageButton.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                return false;
+            }
+        });
         profileImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Get sharedPreferences
+                SharedPreferences myPreferences = v.getContext().getSharedPreferences("app",0);
+                SharedPreferences.Editor editor = myPreferences.edit();
+                editor.putString("profileKey", profile.getProfileKey());
+                editor.putInt("profileId", profile.getProfileId());
+                editor.commit();
+
+                Intent catalog = new Intent(itemView.getContext(), CatalogActivity.class);
+                itemView.getContext().startActivity(catalog);
                 Toast.makeText(itemView.getContext(), "Profile "+profile.getProfileName()+" loading", Toast.LENGTH_LONG).show();
             }
         });
