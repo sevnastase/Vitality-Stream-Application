5bd279d6364cc7e296302227b62768346abcebb9
Ricky van Rijn <rickyvanrijn@gmail.com>
2021-05-10 20:39:20 +0200

    Removed redundant global var.

--- app/src/main/java/com/videostreamtest/utils/ApplicationSettings.java
+++ app/src/main/java/com/videostreamtest/utils/ApplicationSettings.java
@@ -10,6 +10,5 @@ public class ApplicationSettings {
     public final static String COMMUNICATION_INTENT_FILTER = "com.fitstream.sensor.DATA";
     public static final String PRAXCLOUD_URL = "http://188.166.100.139:8080";
     public static final String DEFAULT_LOCAL_MOVIE_STORAGE_FOLDER = "/Praxtour";
-    public static final String DEFAULT_LOCAL_MOVIEPARTS_STORAGE_FOLDER = "/Praxtour";
     public static final String DEFAULT_LOCAL_SOUND_STORAGE_FOLDER = "/Sound";
 }
