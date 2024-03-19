15d0d8b19a10bf27481811f735d79e2b39d1ece6
Greg <vesz.gergo@gmail.com>
2024-03-19 10:47:22 +0100

    Updated BluetoothDeviceBroadcastListener implementation to use threads instead of AsyncTasks

--- app/src/main/java/com/videostreamtest/receiver/BluetoothDeviceBroadcastListener.java
+++ app/src/main/java/com/videostreamtest/receiver/BluetoothDeviceBroadcastListener.java
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 
 public class BluetoothDeviceBroadcastListener extends BroadcastReceiver {
@@ -12,33 +14,42 @@ public class BluetoothDeviceBroadcastListener extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, intent.getAction());
+
         final PendingResult pendingResult = goAsync();
-        final Task asyncTask = new Task(pendingResult, intent);
-        asyncTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+        Handler mainHandler = new Handler(Looper.getMainLooper());
+        Task task = new Task(pendingResult, intent, mainHandler);
+        Thread thread = new Thread(task);
+
+        thread.start();
     }
 
-    private static class Task extends AsyncTask<String, Integer, String> {
+    private static class Task implements Runnable {
 
         private final PendingResult pendingResult;
         private final Intent intent;
+        private final Handler handler;
 
-        private Task(PendingResult pendingResult, Intent intent) {
+        private Task(PendingResult pendingResult, Intent intent, Handler handler) {
             this.pendingResult = pendingResult;
             this.intent = intent;
+            this.handler = handler;
         }
 
         @Override
-        protected String doInBackground(String... strings) {
+        public void run() {
             Log.d(TAG, intent.getAction());
 
-            return intent.getAction();
-        }
+            // Simulating some background task
+            String result = intent.getAction();
 
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            // Must call finish() so the BroadcastReceiver can be recycled.
-            pendingResult.finish();
+            // Once the background task is done, update UI on the main thread
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    // Must call finish() so the BroadcastReceiver can be recycled.
+                    pendingResult.finish();
+                }
+            });
         }
     }
 }
