8b5400e7b915ac7c887e28409980492145369180
Greg <vesz.gergo@gmail.com>
2025-03-02 18:51:32 +0100

    Updated documentation

--- app/src/main/java/com/videostreamtest/utils/ApplicationSettings.java
+++ app/src/main/java/com/videostreamtest/utils/ApplicationSettings.java
@@ -29,14 +29,16 @@ public class ApplicationSettings {
      * <p>
      *     IMPORTANT: {@code localhost} would point to the device actually running the app.
      *     Here are the correct IPs for development/testing:
-     *     Running a VM on your PC/laptop: {@code 10.0.2.2}.
-     *     Physical device: find the IPv4 address of your PC/laptop: {@code 192.168.x.x}.
-     *     You can set the {@code port} to whichever you exposed for your test/dev backend.
+     *     <ul>
+     *      <li> Running a VM on your PC/laptop: {@code 10.0.2.2}. </li>
+     *      <li> Physical device: find the IPv4 address of your PC/laptop: {@code 192.168.x.x}. </li>
+     *     </ul>
+     *     <p> You can set the {@code port} to whichever you exposed for your test/dev backend. </p>
      * </p>
      */
     public static final String PRAXCLOUD_API_URL_TEST = "http://10.0.2.2:8081";
 
-    public static final String PRAXCLOUD_API_URL = PRAXCLOUD_API_URL_TEST;
+    public static final String PRAXCLOUD_API_URL = PRAXCLOUD_API_URL_PROD;
     public static final String PRAXCLOUD_MEDIA_URL = "https://media.praxcloud.eu";
 
     //STANDALONE
