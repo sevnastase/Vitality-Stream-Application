8ab4e9c71695293a0013003bc916dcf48b456cdf
veszgergo <vesz.gergo@gmail.com>
2025-03-31 12:42:28 +0200

    Changed focus behavior

--- app/src/main/java/com/videostreamtest/ui/phone/productpicker/fragments/settings/SettingsFragment.java
+++ app/src/main/java/com/videostreamtest/ui/phone/productpicker/fragments/settings/SettingsFragment.java
@@ -63,13 +63,12 @@ public class SettingsFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
     }
 
-        button.setBackgroundResource(R.color.light_blue_600_focused);
+    private void focus(Button button) {
         button.requestFocus();
         button.setPaintFlags(button.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
     }
 
     private void unfocus(Button button) {
-    private void deselect(Button button) {
         button.setPaintFlags(button.getPaintFlags() & ~Paint.UNDERLINE_TEXT_FLAG);
     }
 
