1c8cd158b43ea75c9288c3e7e6f1926e6c53fa15
veszg <vesz.gergo@gmail.com>
2025-11-12 10:03:08 +0100

    Added boot-on-start update for Android 14

--- app/src/main/java/com/videostreamtest/receiver/OnBootCompletedBroadcastReceiver.java
+++ app/src/main/java/com/videostreamtest/receiver/OnBootCompletedBroadcastReceiver.java
@@ -9,10 +9,13 @@ import android.provider.Settings;
 import android.util.Log;
 
 import com.videostreamtest.R;
+import com.videostreamtest.service.boot_on_start.BootStartupService;
 import com.videostreamtest.ui.phone.splash.SplashActivity;
 import com.videostreamtest.utils.ApplicationSettings;
 
 import static android.os.Build.VERSION_CODES.M;
+
+import androidx.core.content.ContextCompat;
 
 public class OnBootCompletedBroadcastReceiver extends BroadcastReceiver {
 
@@ -21,7 +24,7 @@ public class OnBootCompletedBroadcastReceiver extends BroadcastReceiver {
         final String action = intent.getAction();
 
         if(action != null) {
-            if (Intent.ACTION_BOOT_COMPLETED.equals(action)
+            if ((Intent.ACTION_BOOT_COMPLETED.equals(action) || Intent.ACTION_REBOOT.equals(action))
                     && ApplicationSettings.START_ON_BOOT) {
 
                 // Code to handle BOOT COMPLETED EVENT
@@ -30,18 +33,21 @@ public class OnBootCompletedBroadcastReceiver extends BroadcastReceiver {
                  * Settings > Apps > Special App Access > Display over other apps > Praxtour (set switch on)
                  */
 
-                final Intent splashActivity = new Intent(context, SplashActivity.class);
-                splashActivity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                context.startActivity(splashActivity);
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                    final Intent splashActivity = new Intent(context, SplashActivity.class);
+                    splashActivity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.startActivity(splashActivity);
+                } else {
+                    Intent serviceIntent = new Intent(context, BootStartupService.class);
+                    ContextCompat.startForegroundService(context, serviceIntent);
+                }
 
-                if (Build.VERSION.SDK_INT > M) {
-                    if (Settings.canDrawOverlays(context)) {
-                        Log.e(this.getClass().getSimpleName(), "Value SplashActivity 1: true");
-                    } else {
-                        Log.e(this.getClass().getSimpleName(), "Value SplashActivity 1: "+Settings.canDrawOverlays(context));
-                        Log.e(this.getClass().getSimpleName(), "Value SplashActivity 2: "+Build.VERSION.SDK_INT);
-                        Log.e(this.getClass().getSimpleName(), "Value SplashActivity 2: "+context.getString(R.string.app_name));
-                    }
+                if (Settings.canDrawOverlays(context)) {
+                    Log.e(this.getClass().getSimpleName(), "Value SplashActivity 1: true");
+                } else {
+                    Log.e(this.getClass().getSimpleName(), "Value SplashActivity 1: " + Settings.canDrawOverlays(context));
+                    Log.e(this.getClass().getSimpleName(), "Value SplashActivity 2: " + Build.VERSION.SDK_INT);
+                    Log.e(this.getClass().getSimpleName(), "Value SplashActivity 2: " + context.getString(R.string.app_name));
                 }
             }
         }
