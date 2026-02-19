33734996a71aaa1cbc43b37f10e7297aff3afd6e
veszg <vesz.gergo@gmail.com>
2026-02-19 18:13:57 +0100

    Added PraxConstants to collect constants into one file (future task)

---
+++ app/src/main/java/com/videostreamtest/constants/PraxConstants.java
@@ -0,0 +1,11 @@
+package com.videostreamtest.constants;
+
+public class PraxConstants {
+    public static final String EXTRA_ACCOUNT_TOKEN = "com.videostreamtest.EXTRA_ACCOUNT_TOKEN";
+    public static final String EXTRA_FIRST_LOGIN = "com.videostreamtest.EXTRA_FIRST_LOGIN";
+
+    public static class ApkUpdate {
+        public static final String ACTION_UPDATE = "com.videostreamtest.ACTION_UPDATE";
+        public static final String EXTRA_PACKAGE_NAMES = "com.videostreamtest.EXTRA_PACKAGE_NAME";
+    }
+}
