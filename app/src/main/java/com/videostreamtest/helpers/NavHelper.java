7606cdf772df6dbf81c95e9df6c6786c52a1714f
veszg <vesz.gergo@gmail.com>
2026-05-10 11:56:19 +0200

    Added: old APKs can carry over account now to Launcher

--- app/src/main/java/com/videostreamtest/helpers/NavHelper.java
+++ app/src/main/java/com/videostreamtest/helpers/NavHelper.java
@@ -1,10 +1,12 @@
 package com.videostreamtest.helpers;
 
 import static com.videostreamtest.constants.PraxConstants.ApkUpdate.PRAXTOUR_LAUNCHER_PACKAGE_NAME;
+import static com.videostreamtest.constants.PraxConstants.IntentExtra.EXTRA_ACCOUNT_TOKEN;
 import static com.videostreamtest.constants.PraxConstants.IntentExtra.EXTRA_LOGOUT;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 
 import androidx.annotation.NonNull;
@@ -17,12 +19,18 @@ import com.videostreamtest.ui.phone.splash.SplashActivity;
 public class NavHelper {
     public static void openPraxtourLauncher(@NonNull Activity activity, boolean logout, @Nullable PraxCallbacks.OnFailureCallback onFailureCallback) {
         activity.runOnUiThread(() -> {
+            String savedApikey = null;
+            if (!logout) {
+                savedApikey = activity.getSharedPreferences("app", Context.MODE_PRIVATE).getString("apikey", null);
+            }
+
             Intent installerIntent = activity.getPackageManager().getLaunchIntentForPackage(PRAXTOUR_LAUNCHER_PACKAGE_NAME);
             if (installerIntent != null) {
                 installerIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
                         | Intent.FLAG_ACTIVITY_CLEAR_TOP
                         | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
                 installerIntent.putExtra(EXTRA_LOGOUT, logout);
+                installerIntent.putExtra(EXTRA_ACCOUNT_TOKEN, savedApikey);
                 activity.startActivity(installerIntent);
                 activity.finishAffinity();
             } else {
